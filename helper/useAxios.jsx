2b0522fb0eb55775beec7f8e1c5893a8e2b7d1d2
developer110_Atiq <developer109.fzeetechz@gmail.com>
2025-03-08 17:18:34 +0530

    --change in baseUrl

--- helper/useAxios.jsx
+++ helper/useAxios.jsx
@@ -5,9 +5,9 @@ import { useAuth } from '../context/AuthContext';
 
 
 // export const baseUrl="http://192.168.83.124:3031"
-// export const baseUrl='https://mplbackend-5yl2.onrender.com'
+export const baseUrl='https://mplbackend-5yl2.onrender.com'
 // export const baseUrl="http://192.168.29.174:3031"
-export const baseUrl="http://192.168.1.18:3031"
+// export const baseUrl="http://192.168.1.18:3031"
 
  
 
